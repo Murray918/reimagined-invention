f95712fea639f1e60842db60d32b3433246c46a6
Johnathan <reddjohnathan@gmail.com>
2018-12-20 16:31:22 -0600

    Added more elements. My power grows with my dedication and hard work.

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 
 
 let $container = $("#container")
-console.log($container)
-let $h1 = "<h1>Hogwarts</h1>"
+//console.log($container)
+let $h1 = "<h1>Hogwarts</h1>"+
+          "<h2>Johnathan</h2>"+
+          "<h3>Gryffindor</h3>"+
+          "<h4 class = 'hippo'>Steve</h4>"+
+          "<h4>Hornbeam wand with Dragon's breath</h4>"
+
+
+
 $container.append($h1)
+
 
 
 
