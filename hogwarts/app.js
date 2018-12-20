180e716c107a70c934d4c71f9a682bfe02ef3cc7
pittpir <pittpir@gmail.com>
2018-12-20 17:38:28 -0600

    Added more elements. My power grows with my dedication and hard work.

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -21,6 +21,34 @@ h1.append("Hogwarts");
 container.append(h1);
 
 
+//- h2 element with your name
+//- h3 element with your house (Hufflepuff, Gryffindor, Ravenclaw or Slytherin )
+//- h4 element with your pet's name
+//- this h4 element should have a class with a value of your pet type (owl, cat, toad, etc)
+//- h4 element with your wand (You can make it whatever you want. Need help coming up with one? Here is one of many websites to help you find your wand playbuzz.com )
+//- In your style.css add an attribute of font-family: fantasy; to all elements that have a class that matches your pet's class
+
+let h2 = $('<h2></h2>');
+h2.append("Christopher");
+container.append(h2);
+
+let h3 = $('<h3></h3>');
+h3.append("Ravenclaw");
+container.append(h3);
+
+let h4 = $('<h4></h4>');
+h4.append("Fred").addClass("bunny");
+container.append(h4);
+
+let h4Wand = $('<h4></h4>');
+h4Wand.append("MegaSwampGator Wand");
+container.append(h4Wand);
+
+
+
+
+
+//h4:eq(0)
 
 /*
 $(document).ready(function() {
