ef0d11bb387c61fbae783b327a6e9e6504d7a73f
Jacob Werbin <jwerbin95@gmail.com>
2018-12-20 16:45:22 -0600

    Added list elements. That code didn't seem very DRY. I wonder if Hungry for More will have me DRY it up a bit.

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 let $container = $("#container")
-let $h1 = "<h1>Hogwarts</h1>"+
+let $headings = "<h1>Hogwarts</h1>"+
 		  "<h2>Jacob<h2>"+
 		  "<h3>Slytherin<h3>"+
 		  "<h4 class='Elephant'>Dumbledork<h4>"+
 		  "<h4>Hawthorn Wand with Dragon Heartstring Core<h2>"
-$container.append($h1)
+$container.append($headings)
+let $list = "<ul storage='trunk'>"+
+				"<li>Butter Beer</li>"+
+				"<li class='secret'>Invisibility Cloak</li>"+
+				"<li class='secret'>Magic Map</li>"+
+				"<li class='secret'>Time Turner</li>"+
+				"<li class='Elephant'>leash</li>"+
+				"<li>Bertie Bott's Every Flavor [Jelly] Beans</li>"+
+			"</ul>"
+$container.append($list)
