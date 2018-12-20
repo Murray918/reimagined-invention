7040c9aa47dad75c83aa104a232dd99f22ce4b3a
pittpir <pittpir@gmail.com>
2018-12-20 15:39:05 -0600

    Initial Commit with 3 files

---
+++ hogwarts/app.js
@@ -0,0 +1,23 @@
+const $ = jQuery;
+
+const array = ["Hello", "Goodbye", "Party Time"];
+
+const html = `
+<div>
+
+
+
+
+
+
+
+`
+
+
+const list = `<ul><ul>`
+
+
+//let x = "<p>Blah</p>";
+//$('<p></p>').append(x);
+
+$('#place-to-put-things').append(html).addClass('party')
