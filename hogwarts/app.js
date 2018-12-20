0c7c5cccfc4d14c9377b5307068e5ae648b5af74
pittpir <pittpir@gmail.com>
2018-12-20 17:00:03 -0600

    I added my first element and it was fun

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,23 +1,36 @@
-const $ = jQuery;
+//const $ = jQuery;
 
-const array = ["Hello", "Goodbye", "Party Time"];
+//const array = ["Hello", "Goodbye", "Party Time"];
 
 //const html = `
-//<div>
-
-
-
-
-
-
-
+//<h1></h1>
 //`
+//const list = `<ul><ul>`
+
+//- Query for your div with the id of container and set it to a variable named $container
+//- console.log $container
+//- Create an <h1> element and set it to a variable called $h1 and console log it
+//- Add some text inside the h1 element. Example text: 'Hogwarts'
+//- Why isn't your $h1 it appearing on your page?
+
+let container = $('#container');
+console.log(container);
+
+let h1 = $('<h1></h1>');
+h1.append("Hogwarts");
+container.append(h1);
 
 
-const list = `<ul><ul>`
 
+/*
+$(document).ready(function() {
 
+   $("div").click(function() {
+      alert("Hello, world!");
+   });
+});
+*/
+
+//$('#container').append(html).addClass('party')
 //let x = "<p>Blah</p>";
 //$('<p></p>').append(x);
-
-$('#place-to-put-things').append(html).addClass('party')
