15f8ec9927378d82244bef56b26c0afc49fa8a01
Jacob Werbin <jwerbin95@gmail.com>
2018-12-20 16:29:57 -0600

    🔴 The commit message should read:

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 let $container = $("#container")
-console.log($container)
-let $h1 = "<h1>Hogwarts</h1>"
+let $h1 = "<h1>Hogwarts</h1>"+
+		  "<h2>Jacob<h2>"+
+		  "<h3>Slytherin<h3>"+
+		  "<h4 class='Elephant'>Dumbledork<h4>"+
+		  "<h4>Hawthorn Wand with Dragon Heartstring Core<h2>"
 $container.append($h1)
