e11e125a3e335c1f15fd5c0a42d4f243708a80d0
Andrew Aluko <aluko17@Andrews-MacBook-Air.local>
2018-12-21 13:44:47 -0600

    Added more elements. My power grows with my dedication and hard work.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 
 let container = $('#container')
 let $h1 = '<h1>Hogwarts</h1>'
-console.log($h1)
-container.append($h1)
+
+let $h2 = '<h2> Andrew </h2>'
+let $h3 = '<h3> Ravenclaw </h3>'
+let $h4 = '<h4 class="dog"> Mack </h4><h4> Twisted Holly Wand</h4>'
+
+container.append($h1).append($h2).append($h3).append($h4)
+console.log($h1, $h2, $h3, $h4)
