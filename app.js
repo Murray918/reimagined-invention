6cfe909f6a649606a3b4882dc8d955230c062217
Jacob Werbin <jwerbin95@gmail.com>
2018-12-20 19:56:38 -0600

    I realize I can make things that are increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 let $container = $("#container")
-let $headings = "<h1>Hogwarts</h1>"+
-		  "<h2>Jacob<h2>"+
-		  "<h3>Slytherin<h3>"+
-		  "<h4 class='Elephant'>Dumbledork<h4>"+
-		  "<h4>Hawthorn Wand with Dragon Heartstring Core<h2>"
-$container.append($headings)
-let $list = "<ul storage='trunk'>"+
-				"<li>Butter Beer</li>"+
-				"<li class='secret'>Invisibility Cloak</li>"+
-				"<li class='secret'>Magic Map</li>"+
-				"<li class='secret'>Time Turner</li>"+
-				"<li class='Elephant'>leash</li>"+
-				"<li>Bertie Bott's Every Flavor [Jelly] Beans</li>"+
-			"</ul>"
-$container.append($list)
+let $headings = ["<h1>Hogwarts</h1>",
+		  		 "<h2>Jacob</h2>",
+		    	 "<h3>Slytherin</h3>",
+		  		 "<h4>Dumbledork</h4>",
+		  		 "<h4>Hawthorn Wand with Dragon Heartstring Core</h4>" ]
+let orderedList = ["<ul storage='trunk'>", 
+				   "<li>Butter Beer</li>", 
+				   "<li>Invisibility Cloak</li>", 
+				   "<li>Magic Map</li>",  
+				   "<li>Time Turner</li>", 
+				   "<li>leash</li>", 
+				   "<li>Bertie Bott's Every Flavor [Jelly] Beans</li>", 
+				   "</ul>" ]	
+for(let heading of $headings) {
+	$container.append(heading)
+}		
+for(let item of orderedList) {
+	$container.append(item)
+}
+for(let i = 1;i <= 3; i++) {
+	$(`li:eq(${i})`).addClass("secret")
+}
+$("li:eq(4), h4:eq(0)").addClass("Elephant")
