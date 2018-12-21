1beefbde3b44e06280617c90a61e3f5053829ce5
ohheyitslance <ohheyitslance@ohheyitslances-MacBook.local>
2018-12-21 13:46:08 -0600

    Added more elements. My power grows with my dedication and hard work.

---
+++ app.js
@@ -0,0 +1,13 @@
+let container = $('#container')
+let h1 = '<h1>Hogwarts</h1>'
+container.append(h1)
+
+let h2 = '<h2>Lance</h2'
+container.append(h2)
+let h3 = '<h3> Gryffindor(?)</h3>'
+container.append(h3)
+let h4 = '<h4 class = "dog">Jadah Pinkett Smith</h4><h4>Aspen Wood, Dragon Core, 11 inches</h4>'
+container.append(h4)
+let ul =
+
+    console.log(container)
