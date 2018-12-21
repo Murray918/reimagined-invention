08f292ac3fdde2a990c7091ca4ec7389dc532fff
Sithiz <Joshklaas1@gmail.com>
2018-12-21 14:00:13 -0600

    Added more elements. My power grows with my dedication and hard work.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 let $container = $('#container')
 let $h1 = '<h1>WE DOIN MAgIC</h1>'
-console.log($container)
-console.log($h1)
+
 $container.append($h1)
+
+let $h2 = '<h2>Novice wiZurd Sithis</h2>'
+let $h3 = '<h3>You are in Gryffindor!</h3>'
+let $h4 = '<h4 class="demon">Ördög the Hungarian shapeshifting demon</h4><h4>Hawthorn Wand with Dragon Heartstring Core</h4>'
+
+
+$container.append($h2).append($h3).append($h4)
+console.log($h2,$h3,$h4)
