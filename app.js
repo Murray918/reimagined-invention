1a1caac7ffe962ed90aa65ffc42e2ca6131f4131
AJ <ahumphries102@gmail.com>
2018-12-20 21:58:41 -0600

    Added more elements. My power grows with my dedication and hard work.

---
+++ app.js
@@ -0,0 +1,7 @@
+let container = $('#container')
+let h1 = '<h1>I DO NOT KNOW WHAT GOES HERE</h1>'
+let h2 = '<h2>AJ Humphries</h2'
+let h3 = '<h3>Slytherin</h3>'
+let h4 = '<h4 class="cat">Kerrigan</h4>'
+console.log(container)
+container.append(h1,h2,h3,h4)
