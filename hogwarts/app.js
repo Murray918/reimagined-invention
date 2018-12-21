580f0165478bb41115adae2cb4fdbfe1f39611ce
pittpir <pittpir@gmail.com>
2018-12-21 14:19:11 -0600

    I realize I can make things that are increasingly more complex with greater ease. Adding order is always more effort than causing chaos.

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -71,11 +71,68 @@ let $list = $("li");
 console.log($list);
 
 $list.eq(1).addClass("secret");
-//$("your-pure-css-selector").eq(index)
-//$( "li:eq(-2)" ).addClass( "foo" )
 $list.eq(2).addClass("secret");
 $list.eq(3).addClass("secret");
 $list.eq(4).addClass("bunny");
+
+
+/*
+Make a table
+Hint: Look under More Examples, <p>Simple table with header</p> at MDN to see the correct HTML element layout
+
+Right above your table add an h5 that says 'Spring 2017'
+Inside the table add a thead element
+Inside the thead element add two th elements
+in the first th add the text Day
+in the second th add the text Classes
+Create a tr element and add two td elements inside.
+in the first td add the day Monday
+in the second td add the classes you are taking ( Herbology, Divination, History of Magic, Charms, Potions, Transfiguration, Defense Against the Dark Arts, Quidditch practice, etc.)
+Create more tr elements with tds inside so that you have Monday - Friday and classes each of those days
+In your style.css file, add a gray 1px solid border around your table, th elements and td elements
+In your style.css file, add a rule that will center your table on the page
+
+
+*/
+
+let p = " \
+<table> \
+    <thead> \
+        <th>Day</th> \
+		<th>Classes</th> \
+    </thead> \
+    <tbody> \
+        <tr> \
+            <th>Monday</th> \
+            <th>Charms</th> \
+        </tr> \
+        <tr> \
+            <th>Tuesday</th> \
+            <th>Potions</th> \
+        </tr> \
+        <tr> \
+            <th>Wednesday</th> \
+            <th>Transfiguration</th> \
+        </tr> \
+        <tr> \
+            <th>Thursday</th> \
+            <th>Defense Against the Dark Arts</th> \
+        </tr> \
+        <tr> \
+            <th>Friday</th> \
+            <th>Divination</th> \
+        </tr> \
+    </tbody> \
+</table>"
+
+let h5 = $('<h5></h5>');
+h5.append("Winter 2018");
+container.append(h5);
+container.append(p);
+
+//let $table = 
+
+
 
 
 //$("#myimg").attr("src", "/jquery/images/jquery.jpg");
