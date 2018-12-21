10705cdc24259f1247d7fd0414e5d2bfe759e599
AJ <ahumphries102@gmail.com>
2018-12-21 15:21:01 -0600

    I realize I can make things that are increasingly more complex with greater ease. Adding order is always more effort than causing chaos.

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ let h2 = '<h2>AJ Humphries</h2'
 let h3 = '<h3>Slytherin</h3>'
 let h4 = '<h4 class="cat">Kerrigan</h4>'
 let ul = '<ul><li class="secret">invisibility cloak</li><li class="secret">magic map</li><li class="secret">time turner</li><li class="cat">leash</li><li>Berti Bots</li></ul>'
+let table = '<h5>Spring 2017</h5><table><thead><th>Day</th><th>Classes</th></thead><tr><td>Monday</td><td>Herbology, Dark Arts</td></tr><tr><td>Tuesday</td></tr><tr><td>Wednesday</td></tr><tr><td>Thursday</td></tr><tr><td>Friday</td></tr></table>'
 console.log(container)
-container.append(h1,h2,h3,h4,ul)
+container.append(h1,h2,h3,h4,ul,table)
