e228e3ca7499f95013c51da4b7587b581ee099a1
ohheyitslance <ohheyitslance@ohheyitslances-MacBook.local>
2018-12-21 14:05:04 -0600

    Added list elements. That code didn't seem very DRY. I wonder if Hungry for More will have me DRY it up a bit.

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ let h3 = '<h3> Gryffindor(?)</h3>'
 container.append(h3)
 let h4 = '<h4 class = "dog">Jadah Pinkett Smith</h4><h4>Aspen Wood, Dragon Core, 11 inches</h4>'
 container.append(h4)
-let ul =
-
+let ul = '<ul storage= "Trunk"><li>Butter Beer</li><li class = "Secret">Invisibility Cloak</li><li class = "Secret">Magic Map</li><li class = "Secret">Time Turner</li><li class ="dog">Leash</li><li>Bertie Botts Every Flavor [Jelly] Beans</li></ul>'
+container.append(ul)
     console.log(container)
