6e87b7630c0c7a11c2b3176bc5bd4a5d57b1c028
Johnathan <reddjohnathan@gmail.com>
2018-12-21 11:44:43 -0600

    I realize I can make things that are increasingly more complex with greater ease. Adding order is always more effort than causing chaos.

--- app.js
+++ app.js
@@ -8,13 +8,22 @@ let h1 = "<h1>Hogwarts</h1>"+
           "<h4>Steve</h4>"+
           "<h4>Hornbeam wand with Dragon's breath</h4>"
 container.append(h1)
-$("h4:eq(0)").addClass('hippo')
 
-let ul = "<ul storage = 'trunk'>"
-          "<li>butter beer</li>"
-          "<li class = 'secret'>invisibility cloak</li>"
-          "<li class = 'secret'>magic map</li>"
-          "<li></li>"
+
+let list = ["<ul storage = 'trunk'>",
+            "<li>butter beer</li>",
+            "<li>invisibility cloak</li>",
+            "<li>magic map</li>",
+            "<li>time turner</li>",
+            "<li>leash</li>",
+            "<li>Bertie Bott's Every Flavor [Jelly] Beans</li>"]
+for(let list2 of list){
+	container.append(list2)
+}
+for(i = 1; i <=3; i++){
+	$(`li:eq(${i})`).addClass('secret')
+}
+$("li:eq(4), h4:eq(0)").addClass('hippo')
 
 
 
