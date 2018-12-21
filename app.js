768b636e158abbd4165701b938e8442ad864a4f4
Andrew Aluko <aluko17@Andrews-MacBook-Air.local>
2018-12-21 15:00:03 -0600

    I realize I can make things that are increasingly more complex with greater ease. Adding order is always more effort than causing chaos.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 let container = $('#container')
 let $h1 = '<h1>Hogwarts</h1>'
 
@@ -6,6 +5,8 @@ let $h2 = '<h2> Andrew </h2>'
 let $h3 = '<h3> Ravenclaw </h3>'
 let $h4 = '<h4 class="dog"> Mack </h4><h4> Twisted Holly Wand</h4>'
 let $unorderedList = '<ul storage="trunk"> <li>butter beer</li> <li class="secret">invisibility cloak</li> <li class="secret">magic map</li> <li class="secret">time turner</li> <li class="dog">leash</li> <li>Bertie Botts Every Flavor [Jelly] Beans.</li></ul>'
+let $h5 = '<h5> Spring 2017 </h5>'
+let $table = '<table class="center"><thead><th>Day</th><th>Classes</th></thead><tr><td>Monday</td><td>Herbology, Divination, History of Magic</td></tr><tr><td>Tuesday</td><td>Quidditch, Charms, Healing</td></tr><tr><td>Wednesday</td><td>Curses, Transmorgrifation, Alchemy</td></tr><tr><td>Thursday</td><td>Defense Against the Dark Arts, Astronomy, Flying</td></tr><tr><td>Friday</td><td>Potions, Care of Magical Creatures, Divination</td></tr></table>'
+container.append($h1).append($h2).append($h3).append($h4)
+container.append($unorderedList).append($h5).append($table)
 
-container.append($h1).append($h2).append($h3).append($h4).append($unorderedList)
-console.log($h1, $h2, $h3, $h4)
