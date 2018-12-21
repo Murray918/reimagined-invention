ee91c35ea7e1d45ff4cdb08c64b66ba4f41d8940
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-21 01:46:14 -0600

    Added more elements. My power grows with my dedication and hard work.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 let container = $('#container');
 let h1 = $('<h1>Hogwarts</h1>');
 container.append(h1);
+let h2 = $('<h2>Tristan</h2>');
+
+let house = $('<h3>Ravenclaw</h3>');
+let pet = $('<h4>Koguma</h4>');
+let wand = $('<h4>Sycamore with phoenix feather core</h4>');
+container.append(house, pet, wand);
+pet.addClass('cat');
