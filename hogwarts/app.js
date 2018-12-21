5df1c49b151a0c3227e2d76e6e8eebe0e2e6b2ff
Jamal Amar <JamalAmar@Jamals-MacBook-Air.local>
2018-12-21 14:04:24 -0600

    added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,5 +1,20 @@
 
 let $container = $('#container')
-let $h1 = '<h1>I DO NOT KNOW WHAT GOES HERE</h1>'
+
+let $h1 = '<h1>Hogwarts</h1>'
+let $h2 = '<h2>Jamal Amar</h2>'
+let $h3 = '<h3>Slytherin</h3>'
+let $h4 = '<h4>Panki</h4>'
+let $h5 = '<h4>silver</h4>'
+
 console.log($container)
-$container.append($h1)
+
+
+$('#container')
+.append($h1)
+.append($h2)
+.append($h3)
+.append($h4)
+.append($h5)
+
+$('h4:first-of-type').addClass("dog")
