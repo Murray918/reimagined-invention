baefdbfb1f328828dfd751e808d6594a67c90e4b
Tristan Bennett <tristanbennett@tristanova.local>
2018-12-21 02:09:28 -0600

    Added list elements. That code didn't seem very DRY. I wonder if Hungry for More will have me DRY it up a bit.

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ let pet = $('<h4>Koguma</h4>');
 let wand = $('<h4>Sycamore with phoenix feather core</h4>');
 container.append(house, pet, wand);
 pet.addClass('cat');
+
+let list = $('<ul storage="trunk"></ul>');
+list.append('<li>butter bear</li>');
+list.append('<li class="secret">invisibility cloak</li>');
+list.append('<li class="secret">magic map</li>');
+list.append('<li class = "secret">time turner</li>');
+list.append("<li>Bertie Bott's Every Flavor [Jelly] Beans</li>");
+list.append('<li class ="cat">leash</li>');
+
+container.append(list);
