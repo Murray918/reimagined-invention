770cff4ec9d7a3fe747a1f6c7e7848bee378e835
Johnathan <reddjohnathan@gmail.com>
2018-12-21 12:21:55 -0600

    The days go by slowly and the years go by too fast

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ let h1 = "<h1>Hogwarts</h1>"+
           "<h4>Hornbeam wand with Dragon's breath</h4>"
 container.append(h1)
 
-
 let list = ["<ul storage = 'trunk'>",
             "<li>butter beer</li>",
             "<li>invisibility cloak</li>",
@@ -24,6 +23,15 @@ for(i = 1; i <=3; i++){
 	$(`li:eq(${i})`).addClass('secret')
 }
 $("li:eq(4), h4:eq(0)").addClass('hippo')
+$("h4:eq(1)").remove()
+$("li:eq(0)").remove()
+$("<h4>New wand</h4>").insertAfter("h4")
+$("h4:eq(1)").css({"color": "indigo"})
+//$("h4:eq(0)").detach()
+let oldPet = $("h4:eq(0)").detach()
+container.append(oldPet)
+
+
 
 
 
