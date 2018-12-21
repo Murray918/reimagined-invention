a739d3393828f886d111dc086de7745e45f0b8a2
Sithiz <Joshklaas1@gmail.com>
2018-12-21 15:11:11 -0600

    I realize I can make things that are increasingly more complex with greater ease. Adding order is always more effort than causing chaos.

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ $container.append($h2).append($h3).append($h4)
 let $ul = "<ul><li>butter beer</li><li class='secret'>invisibility cloak</li><li class='secret'>magic map</li><li class ='secret'>time turner</li><li class='demon'>leash</li><li>Bertie Bott's Every Flavor [Jelly] Beans.</li>"
 
 $container.append($ul)
+
+let $table = "<h5>Spring 2017</h5><table class='center'><thread><th>Day</th><th>Classes</th></thread><tr><td>MonDay</td><td>Dark Arts</td>	</tr><tr><td>TuesDay</td><td>Summoning</td> </tr><tr><td>WednesDay</td><td>Transfiguration</td></tr><tr><td>ThursDay</td><td>Divination</td></tr><tr><td>FriDay</td><td>Curses</td></tr></table>"
+
+$container.append($table)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
