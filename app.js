06a17cbe11eefc89d100d2ed0d309b9c52fc59f3
Jacob Werbin <jwerbin95@gmail.com>
2018-12-20 20:30:42 -0600

    The days go by slowly and the years go by too fast

--- app.js
+++ app.js
@@ -22,3 +22,12 @@ for(let i = 1;i <= 3; i++) {
 	$(`li:eq(${i})`).addClass("secret")
 }
 $("li:eq(4), h4:eq(0)").addClass("Elephant")
+$("ul").addClass("itemList")
+$("h4:eq(1)").remove()
+$("li:eq(0)").remove()
+$("<h4>Super Wand With USB 3.0 and Wireless Conectivity</h4>").insertAfter(".Elephant:eq(0)")
+$("h4:eq(1)").css({"color" : "indigo"})
+let petAdventure = $(".Elephant:eq(0)").detach()
+$container.append(petAdventure)
+let petReturn = $(".Elephant:eq(1)").detach()
+$(petReturn).insertAfter("h3")
