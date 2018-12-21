837fb8b13ebaaddbc0710c7b0c198b6b3a4d99b4
Sithiz <Joshklaas1@gmail.com>
2018-12-21 16:56:04 -0600

    A cauliflower is a cabbage with a college education - Mark Twain

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ $container.append($h1)
 
 let $h2 = '<h2>Novice wiZurd Sithis</h2>'
 let $h3 = '<h3>You are in Gryffindor!</h3>'
-let $h4 = '<h4 class="demon">Ördög the Hungarian shapeshifting demon</h4><h4>Hawthorn Wand with Dragon Heartstring Core</h4>'
+let $h4 = '<h4 class="demon">Ördög the Hungarian shapeshifting demon</h4><h4 class="wand">Hawthorn Wand with Dragon Heartstring Core</h4>'
 
 
 $container.append($h2).append($h3).append($h4)
 
-let $ul = "<ul><li>butter beer</li><li class='secret'>invisibility cloak</li><li class='secret'>magic map</li><li class ='secret'>time turner</li><li class='demon'>leash</li><li>Bertie Bott's Every Flavor [Jelly] Beans.</li>"
+let $ul = "<ul><li class='remove'>butter beer</li><li class='secret'>invisibility cloak</li><li class='secret'>magic map</li><li class ='secret'>time turner</li><li class='demon'>leash</li><li>Bertie Bott's Every Flavor [Jelly] Beans.</li>"
 
 $container.append($ul)
 
@@ -18,9 +18,13 @@ let $table = "<h5>Spring 2017</h5><table class='center'><thread><th>Day</th><th>
 
 $container.append($table)
 
-
-
-
+$('h4').remove('.wand')
+$('li').remove('.remove')
+$('h4').after('<h4>WaNd To eNd AlL wAnDs</h4>')
+$('h4').last().css({'color':'green'})
+$('ul li.secret').hide('slow').delay(2000).show(2000)
+$('ul li').eq(3).addClass('cabbage')
+$('ul li').eq(3).removeClass('cabbage')
 
 
 
