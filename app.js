2101afff911aa0f969454d172bcfbdf05571d639
AJ <ahumphries102@gmail.com>
2018-12-20 22:10:36 -0600

    Added more elements. My power grows with my dedication and hard work.

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ let h1 = '<h1>I DO NOT KNOW WHAT GOES HERE</h1>'
 let h2 = '<h2>AJ Humphries</h2'
 let h3 = '<h3>Slytherin</h3>'
 let h4 = '<h4 class="cat">Kerrigan</h4>'
+let ul = '<ul><li class="secret">invisibility cloak</li><li class="secret">magic map</li><li class="secret">time turner</li><li class="cat">leash</li><li>Berti Bots</li></ul>'
 console.log(container)
-container.append(h1,h2,h3,h4)
+container.append(h1,h2,h3,h4,ul)
