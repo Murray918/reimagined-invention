75209ca1e06704b3553b77fbcb53f1c74bfe1a66
pittpir <pittpir@gmail.com>
2018-12-22 14:07:44 -0600

    A cauliflower is a cabbage with a college education - Mark Twain

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -93,6 +93,8 @@ In your style.css file, add a gray 1px solid border around your table, th elemen
 In your style.css file, add a rule that will center your table on the page
 */
 
+
+//here is what the table looks like in normal html
 let p = " \
 <table> \
     <thead> \
@@ -123,12 +125,34 @@ let p = " \
     </tbody> \
 </table>"
 
+//here is building the table in jQuery using loops
+
+let header = ["Day", "Classes"];
+let day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+let classes = ["Herbology", "Divination", "History of Magic", "Charms", "Potions", "Transfiguration", "Defense Against the Dark Arts", "Quidditch practice"]
+
+let $header = ""
+for (let head of header)
+{
+	$header = $header + `<th>${head}</th>`;
+}
+//console.log($header);
+
+let $days1 = "";
+for (let days of day)
+{
+	let randomClass = classes.splice(Math.floor(Math.random() * classes.length),1);  //randomly select a class not not the same class
+	$days1 = $days1 + `<tr><th>${days}</th><th>${randomClass}</th></tr>`;
+}
+//console.log($days1);
+
+let concatTable = $("<table><thead>" + $header + "</thead><tbody>" + $days1 + "</tbody></table>")
+
 let h5 = $('<h5></h5>');
-h5.append("Winter 2018");
+h5.append("Fall 2018");  //changed this to fall becuase I will change this to winter below.
 container.append(h5);
-container.append(p);
-
-
+container.append(p);  //this one is the hand made table
+container.append(concatTable);  //this one is the loop made table
 /*
 Things get interesting
 
@@ -146,15 +170,44 @@ $( "h4" ).eq(-1).remove()
 $( "li" ).remove( ":contains('beer')" );
 $( "<h4>My New Wand</h4>" ).insertAfter( $( ".bunny" ).eq(0));
 $( "h4" ).eq(1).css("color", "blue");
-$( "h4" ).filter(".bunny").text("");
-$( "h4" ).filter(".bunny").append("Fred Came Back");
+//$( "h4" ).filter(".bunny").text("");
+//$( "h4" ).filter(".bunny").detach();
+
+//this will place this in a queue and then run this function after everything lse is done.  Basically, showing I am moving my pet
+window.setTimeout(() =>{ $( "h4" ).filter(".bunny").insertAfter( $( "table:last" )); },3000); 
+ 
+//let spy = $('<h4></h4>');
+//spy.append("Fred").addClass("bunny");
+//container.append(spy);
+
+window.setTimeout(() =>{ $( "h4" ).filter(".bunny").insertAfter( $( "h3" ) ); },5000); 
+ 
 
 
+/*
+Nosey roommate alert! hide (use jQuery method hide) to hide all your belongings with a class of secret (give an argument of 'slow' - to see this function in action)
+Nosey roommate falls asleep 2 seconds later (chain the jQuery method delay on your hide method (give argument of at least 2000) to prevent showing your secret elements too soon.
+Use jQuery method show to reveal all of your belongings with a class of secret (give an argument of 'slow' - to see this function in action)
+Accident! You transmogrified your pet's leash into half cabbage
+add the class cabbage to your pet's leash. Do not replace your pet's leash's original class. Your pet, which also has the same class should remain unaffected
+add an attribute of color: chartreuse; in your style.css for all elements that have a class of cabbage
+Fix your pet's leash by removing the class of cabbage (be sure to keep your pet's leash's original class)
+*/
+$( ".secret" ).hide(2000).delay(2000);  //in milliseconds
+$( ".secret" ).show(2000);  //in milliseconds
 
+//leash will turn green because it has the cabbage class.  It will then go back to default due to losing the class.
+$( "li" ).filter(".bunny").addClass("cabbage");
+window.setTimeout(() =>{ $( "li" ).filter(".bunny").removeClass( "cabbage" ); },5000); 
+ 
+/*
+Though your time at Hogwarts is nearly over, your journey of learning has just begun
 
-
-
-
+Update your class schedule to read 'Fall 2018'
+Celebrate by buying more butter beer! Append a list item with the text 'Butter beer' as the first list item inside your unordered list with the value of trunk
+Whoops! You broke your trunk when you stood on it while singing karaoke. Get a new storage container for your stuff; replace the unodered list's property of trunk with a new property of chest
+Add some CSS to your page. Feel free to experiment and make this page your own
+*/
 
 
 
