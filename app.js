4c571cd6d2894a5f3dadffbcf8cc0aa2f52ea91d
Julius Camarillo <julcamarillo17@gmail.com>
2019-03-05 17:28:26 -0600

    A cauliflower is a cabbage with a college education

--- app.js
+++ app.js
@@ -1,70 +1,68 @@
-
-//how to target a div 
 let container = $("#container")
-let h1 = '<h1> HARRY P </h1>'+
- 		 '<h2> Jules C </h2>'+
- 		 '<h3> Slytherin 4 LYFE</h3>'+
- 		 '<h4> Boo </h4>'+
- 		 '<h4> Ivory Wand</h4>'
+console.log(container)
+let h1 = "<h1>Hogwarts</h1>"+
+          "<h2>Jules C</h2>"+
+          "<h3>Slytherin</h3>"+
+          "<h4>Peekay</h4>"+
+          "<h4>Hornbeam wand with Dragon's breath</h4>"
 container.append(h1)
-$("h4:eq(0)").addClass("Cat")
 
-
-let list = '<ul storage="trunk">'+
-			'<li> Butter Beer</li>'+
-			'<li>invisibilty cloak</li>'+
-			'<li>magic map</li>'+
-			'<li>time turner</li>'
-			'<li>leash</li>'+
-			'<li>Bertie Bott\'s Every Flavor[Jelly] beans</li>'+
-			'</ul>'
+let list = "<ul storage = 'trunk'>"+
+            "<li>butter beer</li>"+
+            "<li>invisibility cloak</li>"+
+            "<li>magic map</li>"+
+            "<li>time turner</li>"+
+            "<li>leash</li>"+
+            "<li>Bertie Bott's Every Flavor [Jelly] Beans</li>"+
+            "</ul>"
 container.append(list)
-$("li:eq(1)").addClass("secret")
-$("li:eq(2)").addClass("secret")
-$("li:eq(3)").addClass("secret")
-$("li:eq(4)").addClass("Cat")
 
-let table = '<table>'+
-			'<thead>'+
-				'<th>Day</th>'+
-				'<th>Classes</th>'+
-        	'<tr>' +
-        		'<td>Monday</td>'+
-        		'<td>Herbology</td>'+
-            '</tr>'+
-        	'<tr>' +
-        		'<td>Tuesday</td>'+
-        		'<td>Divination</td>'+
-            '</tr>'+
-            '<tr>' +
-        		'<td>Wednesday</td>'+
-        		'<td>History of Magic</td>'+
-            '</tr>'+
-            '<tr>' +
-        		'<td>Thursday</td>'+
-        		'<td>Charms</td>'+
-            '</tr>'+
-            '<tr>' +
-        		'<td>Friday</td>'+
-        		'<td>Potions</td>'+
-            '</tr>'+
-        	'</thead>'+
-        	'</table>'
-container.append("<h5> Spring 2017</h5>")
+$("li:eq(1), li:eq(2), li:eq(3)").addClass('secret')
+
+
+let table = "<table>"+
+                "<thead>"+
+                        "<th>Day</th>"+
+                        "<th>Classes</th>"+
+                    "<tr>"+
+                        "<td>Monday</td>"+
+                        "<td>Potions</td>"+
+                    "</tr>"+
+                    "<tr>"+
+                        "<td>Tuesday</td>"+
+                        "<td>Herbology</td>"+
+                    "</tr>"+
+                    "<tr>"+
+                        "<td>Wednesday</td>"+
+                        "<td>History of Magic</td>"+
+                    "</tr>"+
+                    "<tr>"+
+                        "<td>Thursday</td>"+
+                        "<td>Charms</td>"+
+                    "</tr>"+
+                    "<tr>"+
+                        "<td>Friday</td>"+
+                        "<td>Quidditch practice</td>"+
+                    "</tr>"+        
+                "</thead>"+
+            "</table>"
+container.append("<h5>Spring 2017</h5>")
 container.append(table)
 
-
-
-
-
-
-
-
-
-
-
-
-
+$("li:eq(4), h4:eq(0)").addClass('hippo')
+$("h4:eq(1)").remove()
+$("li:eq(0)").remove()
+$("<h4>New wand</h4>").insertAfter("h4")
+$("h4:eq(1)").css({"color": "indigo"})
+//$("h4:eq(0)").detach()
+let oldPet = $("h4:eq(0)").detach()
+container.append(oldPet)
+$(".secret").hide('slow').delay(2000).show('slow')
+$("li:eq(3)").addClass('cabbage')
+$("li:eq(3)").removeClass('cabbage')
+$("h5").text('Fall 2018')
+$("[storage*=trunk]").prepend("<li>Butter Beer</li>")
+$("[storage*=trunk]").attr("storage", 'Chest')
 
 
 
